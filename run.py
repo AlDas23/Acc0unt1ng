6e038715d41ee64a67870438f3bbb347cacd61c2
AlDas23 <alex.dascalu1@gmail.com>
2024-08-08 12:18:40 +0300

    Colab branch first commit

--- run.py
+++ run.py
@@ -1,16 +1,9 @@
-from UIs.terminal_UI import *
-from UIs.web_UI import *
+from api.api_run import *
 import argparse
 
 
 def run():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--terminal", action="store_true", help="Run using Terminal UI")
-    args = parser.parse_args()
-    if args.terminal == True:
-        Terminal()
-    else:
-        webUI()
+    api_start()
 
 
 if __name__ == "__main__":
