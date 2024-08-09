278dde85b4769b4662f7933b53899bbb530099db
AlDas23 <alex.dascalu1@gmail.com>
2024-08-09 11:40:49 +0300

    Updates consts paths for colab

--- db_scripts/consts.py
+++ db_scripts/consts.py
@@ -1,8 +1,8 @@
-dbPath = ".\db\Main.db"
-SPVcatIncPath = ".\db\catInc.csv"
-SPVcatExpPath = ".\db\catExp.csv"
-SPVsubcatPath = ".\db\subcat.csv"
-SPVcurrPath = ".\db\curr.csv"
+dbPath = "\content\drive\MyDrive\Acc0unt1ng\Main.db"
+SPVcatIncPath = "\content\drive\MyDrive\Acc0unt1ng\catInc.csv"
+SPVcatExpPath = "\content\drive\MyDrive\Acc0unt1ng\catExp.csv"
+SPVsubcatPath = "\content\drive\MyDrive\Acc0unt1ng\subcat.csv"
+SPVcurrPath = "\content\drive\MyDrive\Acc0unt1ng\curr.csv"
 
 keys = [
     "id",
