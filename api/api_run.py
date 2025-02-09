50a81c853ade0829da621b84db3cb96a45f8295d
AlDas23 <alex.dascalu1@gmail.com>
2025-02-09 13:27:33 +0200

    Added missing %

--- api/api_run.py
+++ api/api_run.py
@@ -517,7 +517,7 @@ def ViewAdvReports():
                 columns = ["Category", "Person bank", "Currency", "Sum"]
             if report_type == "catincrep":
                 data = ReadAdv(report_type, month)
-                columns = ["Category", "Sum RON"]
+                columns = ["Category", "Sum RON", "%"]
             if report_type == "catexprep":
                 data = ReadAdv(report_type, month)
                 columns = ["Category", "Sum RON", "%"]
